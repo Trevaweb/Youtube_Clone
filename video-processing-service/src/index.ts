60ee8085e4809fe4f1bdc60a0a95633732c01950
trevaweb <trevaweb@gmail.com>
2024-12-12 12:52:12 -0600

    setup basic api code to handle video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,16 +1,66 @@
-import express from "express";
+import express from 'express';
 
-//define your app type and port
+
+import { 
+  uploadProcessedVideo,
+  downloadRawVideo,
+  deleteRawVideo,
+  deleteProcessedVideo,
+  convertVideo,
+  setupDirectories
+} from './storage';
+
+// Create the local directories for videos
+setupDirectories();
+
 const app = express();
-const port= 3000;
+app.use(express.json());
 
-//defines a http get route that sends hello world
-app.get("/", (req, res) => {
-    res.send("Hello World");
+// Process a video file from Cloud Storage into 360p
+app.post('/process-video', async (req, res) => {
+
+  // Get the bucket and filename from the Cloud Pub/Sub message
+  let data;
+  try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+    data = JSON.parse(message);
+    if (!data.name) {
+      throw new Error('Invalid message payload received.');
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send('Bad Request: missing filename.');
+  }
+
+  const inputFileName = data.name;
+  const outputFileName = `processed-${inputFileName}`;
+
+  // Download the raw video from Cloud Storage
+  await downloadRawVideo(inputFileName);
+
+  // Process the video into 360p
+  try { 
+    await convertVideo(inputFileName, outputFileName)
+  } catch (err) {
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)
+    ]);
+    return res.status(500).send('Processing failed');
+  }
+  
+  // Upload the processed video to Cloud Storage
+  await uploadProcessedVideo(outputFileName);
+
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ]);
+
+  return res.status(200).send('Processing finished successfully');
 });
 
-//this listens on the defined port and logs that
-app.listen(port, () =>{
-    console.log(
-        `Video processing service listening at http://localhost:${ port }`);
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
